245ef312656373189784d883be897237582218d2
Neer Shrestha <shrestha.neer@hotmail.com>
2023-12-29 22:17:17 +0545

    made model for roles, categories and products

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,16 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'title',
-        'description',
-        'price',
-        'qty',
-        'status',
-        'category_id'
+        'category_name'
     ];
 }
